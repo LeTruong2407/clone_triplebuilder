af8cd6be944b7fbc0424dfc464d2f8ef0de0d3f5
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-06-04 16:05:32 +0700

    delete src dir

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { Core } from './core.js';
 const scoreUI = document.querySelector("#score")
 const highscoreUI = document.querySelector("#high_score")
 const timerUI = document.querySelector("#timer")
-const gameTime = 20
+const gameTime = 60
 window.onload = () =>{
     const app = new Core(function(){
         app.createGame(10, 10);
